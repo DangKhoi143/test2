3271567fdb45b23538e92b6fb9717de910d98288
dang khoi <s4015143@rmit.edu.vn>
2024-06-04 12:48:44 +0700

    yes

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+
+const server = app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
+
+// This is optional, Keep-Alive header can inform the client how long the server is willing to keep the connection open (timeout=N value) before the server will force a close of the connection. Please refer: https://nodejs.org/api/http.html#serverkeepalivetimeout/
+server.keepAliveTimeout = 120 * 1000; // 120 seconds
